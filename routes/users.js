096446ea2d4389fda1b364053d17d5aa8eda97cf
Dexter Crago <jodieanddexter@gmail.com>
2024-05-06 08:35:08 -0600

    Write initial user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+const user_controller = require('../controllers/userController');
+
+router.get('/', user_controller.index);
+
+router.get('/sign-up', user_controller.sign_up_get);
+
+router.post('/sign-up', user_controller.sign_up_post);
+
+router.get('/login', user_controller.login_get);
+
+router.post('/login', user_controller.login_post);
+
+router.get('/join-the-club', user_controller.join_the_club_get);
+
+router.post('/join-the-club', user_controller.join_the_club_post);
 
 module.exports = router;
