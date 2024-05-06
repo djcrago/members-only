e8f521162c8b101277f824582b0309304d42f43e
Dexter Crago <jodieanddexter@gmail.com>
2024-05-06 11:40:24 -0600

    Create LocalStrategy to authenticate login with bcryptjs

--- app.js
+++ app.js
@@ -36,6 +36,35 @@ app.use(limiter);
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.session());
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 // create mongoose connection to database
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
