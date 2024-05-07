cef0ea424baf98633063e3d429f2cb660b22ae96
Dexter Crago <jodieanddexter@gmail.com>
2024-05-07 08:25:43 -0600

    Add comments and clean up app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const createError = require('http-errors');
+// Require all background/utility dependencies
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcryptjs');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
@@ -11,24 +16,22 @@ const limiter = RateLimit({
   max: 30,
 });
 require('dotenv').config();
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcryptjs');
 
+// User model is utilized by passport
 const User = require('./models/user');
 
+// Require routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const messagesRouter = require('./routes/messages');
 
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// setup passportjs
+// Passportjs setup
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.session());
 
@@ -36,14 +39,17 @@ passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
       const user = await User.findOne({ username: username });
+
       if (!user) {
         return done(null, false, { message: 'Incorrect username' });
       }
+
       const match = await bcrypt.compare(password, user.password);
+
       if (!match) {
-        // passwords do not match!
         return done(null, false, { message: 'Incorrect password' });
       }
+
       return done(null, user);
     } catch (err) {
       return done(err);
@@ -56,6 +62,7 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
+
     done(null, user);
   } catch (err) {
     done(err);
@@ -63,9 +70,24 @@ passport.deserializeUser(async (id, done) => {
 });
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
+
   next();
 });
 
+// Create mongoose connection to database
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const mongoDB = process.env.MONGODB_URL;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+// Initialize middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -75,33 +97,23 @@ app.use(compression());
 app.use(helmet());
 app.use(limiter);
 
-// create mongoose connection to database
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-
-const mongoDB = process.env.MONGODB_URL;
-main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect(mongoDB);
-}
-
+// Initialize routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
